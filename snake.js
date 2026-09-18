1480e1014926da246b4c866ad6b44f384f113f1d
Maria <mv9312550@gmail.com>
2026-09-18 13:31:15 +0200

    first commit, so far i have a functionning snake game with a score board and food/poison in the map

---
+++ snake.js
@@ -0,0 +1,148 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const gridSize = 20;
+let snake = [
+	{ x: 120, y: 120 }
+];
+let direction = "RIGHT";
+let a;
+let food = createFood();
+let poison = createPoison();
+let score = 0;
+const scoreDisplay = document.getElementById("scoreValue");
+const startButton = document.getElementById("startButton");
+
+//fct pour verifier que la tete touche pas le corps
+function collision(head, array) {
+	for (let i = 1; i < array.length; i++) {
+		if (head.x === array[i].x && head.y === array[i].y)
+			return true;
+	}
+	return false;
+}
+
+function startGame() {
+	startButton.style.display = "none";
+	a = setInterval(draw, 100);//lance le jeu
+}
+startButton.addEventListener("click", startGame);
+
+function createFood() {
+	let x = Math.floor(Math.random() * (canvas.width / gridSize)) * gridSize;
+	let y = Math.floor(Math.random() * (canvas.height / gridSize)) * gridSize;
+	//check si la pomme est dans dans la meme position que le serpent
+	for (let i = 0; i < snake.length; i++) {
+		if (snake[i].x === x && snake[i].y === y) {
+			return createFood(); //si oui, on relance
+		}
+	}
+	return { x: x, y: y };
+}
+
+function createPoison() {
+	let x = Math.floor(Math.random() * (canvas.width / gridSize)) * gridSize;
+	let y = Math.floor(Math.random() * (canvas.height / gridSize)) * gridSize;
+
+	for (let i = 0; i < snake.length; i++) {
+		if (snake[i].x === x && snake[i].y === y) {
+			return createPoison();
+		}
+	}
+	if (x === food.x && y === food.y) //check si pomme et poison sont pas au meme endroit
+		return createPoison
+	return { x: x, y: y };
+}
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = "#2f352fff";
+	ctx.lineWidth = 1;
+
+	for (let x = 0; x <= canvas.width; x += gridSize) {
+		ctx.beginPath();
+		ctx.moveTo(x, 0);
+		ctx.lineTo(x, canvas.height);
+		ctx.stroke();
+	}
+	for (let y = 0; y <= canvas.height; y += gridSize) {
+		ctx.beginPath();
+		ctx.moveTo(0, y);
+		ctx.lineTo(canvas.width, y);
+		ctx.stroke();
+	}
+	//serpent
+	ctx.fillStyle = "green";
+	for (let i = 0; i < snake.length; i++) {
+		ctx.fillRect(snake[i].x, snake[i].y, gridSize, gridSize);
+	}
+	//pomme
+	ctx.fillStyle = "red";
+	ctx.fillRect(food.x, food.y, gridSize, gridSize);
+	//poison
+	ctx.fillStyle = "black";
+	ctx.fillRect(poison.x, poison.y, gridSize, gridSize);
+	let newHead = { x: snake[0].x, y: snake[0].y };
+
+	if (direction == "RIGHT") newHead.x += gridSize;
+	else if (direction == "LEFT") newHead.x -= gridSize;
+	else if (direction == "UP") newHead.y -= gridSize;
+	else if (direction == "DOWN") newHead.y += gridSize;
+
+	if (newHead.x === food.x && newHead.y === food.y) {
+		score++;
+		scoreDisplay.innerText = score; //mise a jour du text
+		food = createFood();
+		poison = createPoison();
+	} else {
+		snake.pop(); //
+	}
+	snake.unshift(newHead);
+
+	if (newHead.x === poison.x && newHead.y === poison.y) {
+		clearInterval(a);
+		setTimeout(() => {
+			alert("Game over! Cause of death: Poison");
+			resetGame();
+		}, 100);
+	}
+
+	if (newHead.x < 0 || newHead.x >= canvas.width || newHead.y < 0 ||
+		newHead.y >= canvas.height || collision(newHead, snake)) {
+		clearInterval(a);
+		setTimeout(() => {
+			alert("Game over");
+			snake = [{ x: 120, y: 120 }];
+			direction = "RIGHT";
+			score = 0;
+			scoreDisplay.innerText = score;
+			startButton.style.display = "block"; //le bouton revient
+		}, 100);
+	}
+}
+
+function resetGame() {
+	snake = [{ x: 120, y: 120 }];
+	direction = "RIGHT";
+	score = 0;
+	scoreDisplay.innerText = score;
+	food = createFood();
+	poison = createPoison();
+	startButton.style.display = "block";
+	location.reload();
+}
+
+document.addEventListener("keydown", function (event) {
+	if (event.key === "ArrowUp" && direction !== "DOWN") {
+		direction = "UP";
+	} else if (event.key === "ArrowDown" && direction !== "UP") {
+		direction = "DOWN";
+	} else if (event.key === "ArrowLeft" && direction !== "RIGHT") {
+		direction = "LEFT";
+	} else if (event.key === "ArrowRight" && direction !== "LEFT") {
+		direction = "RIGHT";
+	}
+});
+
+//boucle tous les 100ms
+//a = setInterval(draw, 100);
